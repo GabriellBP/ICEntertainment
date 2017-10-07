60d710277a22945361b9dfef2eff8de1c5564044
gbp@ic.ufal.br <gbp@ic.ufal.br>
2017-10-07 15:01:14 -0300

    organizing the structure

--- Server/ICEntertainment/urls.py
+++ Server/ICEntertainment/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
+
+from . import views
 
 urlpatterns = [
     url(r'^$', views.PostList.as_view(), name='post_list'),
