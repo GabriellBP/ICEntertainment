c7a8769b3b3467e76785eb929198ae550ae85e87
gbp@ic.ufal.br <gbp@ic.ufal.br>
2017-10-07 19:07:14 -0300

    implementing the models

--- Server/ICEntertainment/admin.py
+++ Server/ICEntertainment/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(File)
--- Server/ICEntertainment/models.py
+++ Server/ICEntertainment/models.py
@@ -1,25 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-
-class Post(models.Model):
-    author = models.ForeignKey('User')
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to='uploads/imageUsers/', blank=True, null=True)
-    created_date = models.DateTimeField(
-        default=timezone.now)
-    published_date = models.DateTimeField(
-        blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
-
-
 class User(models.Model):
     nickname = models.CharField(max_length=30)
     name = models.CharField(max_length=100)
@@ -29,9 +10,39 @@ class User(models.Model):
     def __str__(self):
         return self.nickname
 
+class Post(models.Model):
+    author = models.ForeignKey('User')
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    image_post = models.ImageField(upload_to='uploads/imageUsers/', blank=True, null=True)
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
 
-class Files(models.Model):
+    def __str__(self):
+        return self.title
+
+class Image(models.Model):
+    post = models.ForeignKey('Post')
+    image = models.ImageField(upload_to='uploads/imageUsers/')
+
+    def __str__(self):
+        return str(self.id)
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post')
+    author = models.ForeignKey('User')
+    comment = models.TextField()
+
+    def __str__(self):
+        return str(self.id)
+
+class File(models.Model):
+    post = models.ForeignKey('Post')
     title = models.CharField(max_length=200)
     description = models.TextField()
     file = models.CharField(max_length=300)
-    post = models.ForeignKey('Post')
+
+    def __str__(self):
+        return self.title
